7b8f6229000b02815d36e3aea1a4153e958b800f
Zakhar <ukhanovza@gmail.com>
2022-07-13 21:13:50 +0300

    added main page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './Header/Header'
 import styles from './App.module.scss'
+import MainInfo from './MainInfo/MainInfo'
 
 function App() {
 
   return (
     <div className={styles.App}>
       <Header/>
+      <MainInfo/>
     </div>
   )
 }
--- src/components/MainInfo/MainInfo.tsx
+++ src/components/MainInfo/MainInfo.tsx
@@ -1,3 +1,23 @@
 import React from "react";
-import styles from './MainInfo.module.scss'
+import styles from "./MainInfo.module.scss";
+import house from "../../imgs/mainhouse.svg"
 
+const MainInfo: any = () => {
+  return (
+    <div className={styles.MainInfo}>
+      <div className={styles.LeftInfo}>
+        <h1>Find your custom matched commercial real estate space.</h1>
+        <p>
+          In oculis quidem exercitus quid ex ea consequatur?
+          <br /> autem vel eum fugiat, quo pertineant ero tibique
+        </p>
+        <div className={styles.LeftInfo__Form}>
+          <input placeholder={`Your email address`} type="e-mail" />
+          <button>Subscribe</button>
+        </div>
+      </div>
+      <img src={house}/>
+    </div>
+  );
+};
+export default MainInfo;
